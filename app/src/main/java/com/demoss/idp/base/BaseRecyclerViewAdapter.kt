1284873155e36848c1f5e44d06632c8c108fbf54
Daniel <mossur15@gmail.com>
2019-01-14 17:51:39 +0200

    updated libraries added extension function for rx to update variable refactored base recycler adapter on AsyncListDiffer

--- app/src/main/java/com/demoss/idp/base/BaseRecyclerViewAdapter.kt
+++ app/src/main/java/com/demoss/idp/base/BaseRecyclerViewAdapter.kt
@@ -3,47 +3,32 @@ package com.demoss.idp.base
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 
-abstract class BaseRecyclerViewAdapter<T,
-        VH : BaseRecyclerViewAdapter.BaseViewHolder<T>,
-        DiffUtilCallback : BaseRecyclerViewAdapter.BaseDiffUtilCallback<T>> : RecyclerView.Adapter<VH>() {
+abstract class BaseRecyclerViewAdapter<T, VH : BaseRecyclerViewAdapter.BaseViewHolder<T>> : RecyclerView.Adapter<VH>() {
 
-    val data: MutableList<T> = mutableListOf()
     abstract val viewHolderFactory: (view: View) -> VH
     abstract val layoutResId: Int
-    abstract val diffUtilCallbackFactory: (oldList: List<T>, newList: List<T>) -> DiffUtilCallback
+    abstract var differ: AsyncListDiffer<T>
 
     // RV Adapter functions ============================================================================================
-    final override fun getItemCount(): Int = data.size
+    final override fun getItemCount(): Int = differ.currentList.size
 
     final override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH = viewHolderFactory(
         LayoutInflater.from(parent.context)
             .inflate(layoutResId, parent, false)
     )
 
-    override fun onBindViewHolder(holder: VH, position: Int) {
-        holder.bindData(data[position])
-    }
+    override fun onBindViewHolder(holder: VH, position: Int) = holder.bindData(differ.currentList[position])
 
     // DiffUtil applying ===============================================================================================
-    fun dispatchData(list: List<T>) {
-        with(DiffUtil.calculateDiff(diffUtilCallbackFactory(data, list))) {
-            data.clear()
-            data.addAll(list)
-            dispatchUpdatesTo(this@BaseRecyclerViewAdapter)
-        }
-    }
+    fun dispatchData(list: List<T>) = differ.submitList(list)
 
     // Abstract classes ================================================================================================
-    abstract class BaseDiffUtilCallback<T>(var oldList: List<T>, var newList: List<T>) : DiffUtil.Callback() {
-        override fun getOldListSize(): Int = oldList.size
-        override fun getNewListSize(): Int = newList.size
-        override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
-            oldList[oldItemPosition]?.hashCode() == newList[newItemPosition]?.hashCode()
-        override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
-            oldList[oldItemPosition] == newList[newItemPosition]
+    abstract class BaseDiffUtilItemCallback<T>() : DiffUtil.ItemCallback<T>() {
+        override fun areItemsTheSame(oldItem: T, newItem: T): Boolean = oldItem == newItem
     }
 
     abstract class BaseViewHolder<T>(val view: View) : RecyclerView.ViewHolder(view) {
--- app/src/main/java/com/demoss/idp/util/RxExtension.kt
+++ app/src/main/java/com/demoss/idp/util/RxExtension.kt
@@ -6,6 +6,7 @@ import io.reactivex.Observable
 import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
+import kotlin.reflect.KMutableProperty
 
 // Default schedulers ==================================================================================================
 
@@ -20,3 +21,8 @@ fun Completable.setDefaultSchedulers(): Completable = this.observeOn(AndroidSche
 
 fun <T> Maybe<T>.setDefaultSchedulers(): Maybe<T> = this.observeOn(AndroidSchedulers.mainThread())
     .subscribeOn(Schedulers.io())
+
+// Other ===============================================================================================================
+
+fun <T> Observable<T>.updateValue(target: KMutableProperty<T>): Observable<T> =
+    this.map { it.apply { target.setter.call(it) } }
